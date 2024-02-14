05f362ca38cff47c3bc1700ee4a14a4ae1c2db11
weihong <weihong2398@gmail.com>
2024-02-14 13:14:29 -0500

    zzz

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,9 +1,24 @@
 <script setup>
+const Sidearm = [
+  {
+    Name: "Ghost",
+    Price: "500",
+  }
+]
+const Main = [ 
+   {
+    Name: "Vandal",
+    Price: "2900",
+   },
+]
 </script>
 
 <template>
   <main>
     <h1>VALORANT LOADOUT</h1>
+    <button>take vandal price</button>
+    <button>take side arm price</button>
+    <button>take armor price</button>
     <TheWelcome />
   </main>
 </template>
