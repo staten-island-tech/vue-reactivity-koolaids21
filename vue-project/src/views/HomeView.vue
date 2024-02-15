4407db1d413ca5b4d068a67e098f61b93e95b1f8
weihong <weihong2398@gmail.com>
2024-02-15 13:09:06 -0500

    zzz

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -3,14 +3,85 @@ const Sidearm = [
   {
     Name: "Ghost",
     Price: "500",
-  }
+  },
+  {
+    Name: "Classic",
+    Price: "0",
+   },
+   {
+    Name: "Frenzy",
+    Price: "450",
+   },
+   {
+    Name: "Shorty",
+    Price: "300",
+   },
+   {
+    Name: "Sherrif",
+    Price: "800",
+   },
 ]
 const Main = [ 
    {
     Name: "Vandal",
     Price: "2900",
    },
+   {
+    Name: "Phantom",
+    Price: "2900",
+   },
+   {
+    Name: "Bulldog",
+    Price: "2050",
+   },
+   {
+    Name: "Guardian",
+    Price: "2250",
+   },
+   {
+    Name: "Spectre",
+    Price: "1600",
+   },
+   {
+    Name: "Spectre",
+    Price: "1600",
+   },
+   {
+    Name: "Bucky",
+    Price: "850",
+   },
+   {
+    Name: "Judge",
+    Price: "1850",
+   },
+   {
+    Name: "Stinger",
+    Price: "1100",
+   },
+   {
+    Name: "Marshal",
+    Price: "950",
+   },
+   {
+    Name: "Outlaw",
+    Price: "2400",
+   },
+   {
+    Name: "Operator",
+    Price: "4700",
+   },
+   {
+    Name: "Ares",
+    Price: "1600",
+    
+   },
+   {
+    Name: "Odin",
+    Price: "3200",
+   },
 ]
+
+console.log,{Main}
 </script>
 
 <template>
