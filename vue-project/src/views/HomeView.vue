3e87b51b018a567f2f626b03e63b14dc73ec800b
koolaids21 <weihong2398@gmail.com>
2024-02-24 15:15:16 -0500

    need to add thing to show total

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -2,95 +2,92 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { ref, computed } from "vue";
-// const Sidearm = [
-//   {
-//     Name: "Ghost",
-//     Price: "500",
-//   },
-//   {
-//     Name: "Classic",
-//     Price: "0",
-//   },
-//   {
-//     Name: "Frenzy",
-//     Price: "450",
-//   },
-//   {
-//     Name: "Shorty",
-//     Price: "300",
-//   },
-//   {
-//     Name: "Sherrif",
-//     Price: "800",
-//   },
-// ];
-// const Main = [
-//   {
-//     Name: "Vandal",
-//     Price: "2900",
-//   },
-//   {
-//     Name: "Phantom",
-//     Price: "2900",
-//   },
-//   {
-//     Name: "Bulldog",
-//     Price: "2050",
-//   },
-//   {
-//     Name: "Guardian",
-//     Price: "2250",
-//   },
-//   {
-//     Name: "Spectre",
-//     Price: "1600",
-//   },
-//   {
-//     Name: "Spectre",
-//     Price: "1600",
-//   },
-//   {
-//     Name: "Bucky",
-//     Price: "850",
-//   },
-//   {
-//     Name: "Judge",
-//     Price: "1850",
-//   },
-//   {
-//     Name: "Stinger",
-//     Price: "1100",
-//   },
-//   {
-//     Name: "Marshal",
-//     Price: "950",
-//   },
-//   {
-//     Name: "Outlaw",
-//     Price: "2400",
-//   },
-//   {
-//     Name: "Operator",
-//     Price: "4700",
-//   },
-//   {
-//     Name: "Ares",
-//     Price: "1600",
-//   },
-//   {
-//     Name: "Odin",
-//     Price: "3200",
-//   },
-// ];
 
-// Main.forEach(Guns);
-// Sidearm.forEach(Pistol);
+const Main = [
+  {
+    Name: "Ghost",
+    Price: "500",
+  },
+  {
+    Name: "Classic",
+    Price: "0",
+  },
+  {
+    Name: "Frenzy",
+    Price: "450",
+  },
+  {
+    Name: "Shorty",
+    Price: "300",
+  },
+  {
+    Name: "Sherrif",
+    Price: "800",
+  },
+  {
+    Name: "Vandal",
+    Price: "2900",
+  },
+  {
+    Name: "Phantom",
+    Price: "2900",
+  },
+  {
+    Name: "Bulldog",
+    Price: "2050",
+  },
+  {
+    Name: "Guardian",
+    Price: "2250",
+  },
+  {
+    Name: "Spectre",
+    Price: "1600",
+  },
+  {
+    Name: "Spectre",
+    Price: "1600",
+  },
+  {
+    Name: "Bucky",
+    Price: "850",
+  },
+  {
+    Name: "Judge",
+    Price: "1850",
+  },
+  {
+    Name: "Stinger",
+    Price: "1100",
+  },
+  {
+    Name: "Marshal",
+    Price: "950",
+  },
+  {
+    Name: "Outlaw",
+    Price: "2400",
+  },
+  {
+    Name: "Operator",
+    Price: "4700",
+  },
+  {
+    Name: "Ares",
+    Price: "1600",
+  },
+  {
+    Name: "Odin",
+    Price: "3200",
+  },
+];
 
-// function addPrice() {
-//   `<p>${Guns.price} + ${Pistol.price}</p>`;
-// }
+const cart = ref([]);
 
-// Define your Vue component
+const additem = (item) => {
+  cart.value.push(item);
+};
+
 const num1 = ref(0);
 const num2 = ref(0);
 
